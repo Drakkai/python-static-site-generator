a00ea1106e0c3d9ffe195da5637dca06c43bbf1e
drakkai <drakkai@linux.pl>
2020-10-05 17:11:52 +0200

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -29,5 +29,5 @@ class Site:
         parser = self.load_parser(path.suffix)
         if parser is not None:
             parser.parse(path, self.source, self.dest)
-        else: 
+        else:
             print("Not Impremented")
